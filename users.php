372287f28fccf31f8cce526e99371e02861f0d18
Gustavo Guimarães <gustavo.cervo14@gmail.com>
2018-11-14 09:38:47 -0200

    funcionalidade nova

---
+++ users.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>ALunossssss</title>
+	<link rel="stylesheet" type="text/css" href="css/fontawesome-all.css">
+	<style type="text/css">
+		body{
+			margin: 10px;
+			padding: 0;
+		}
+		.container-user{
+			width: 90%;
+			padding: 0;
+			margin: auto;
+		}
+		.user{
+			display: inline-block;
+			width: 50%;
+			height: 150px;
+			margin: -2px;
+			text-align: center;
+			color: #fff;
+			font-weight: bold;
+			font-size: 1.3em;
+		}
+		.title{
+			padding: 0;
+			margin: 0;
+		}
+
+		.number{
+			padding: 30px;
+			margin: 0;
+			font-size: 2em;
+		}
+
+		@media only screen and (min-width: 600px) {
+    		.container-user{
+				width: 80%;
+			}
+			.user{
+				width: 25%;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="container-user">
+		<div class="user" style="background: #007bff">
+			<p class="title">Alunos</p>
+			<p class="number">1</p>
+		</div>
+		<div class="user" style="background: #150730">
+			<p class="title">Professores</p>
+			<p class="number">1</p>
+		</div>
+		<div class="user" style="background: #007333">
+			<p class="title">Coordenadores</p>
+			<p class="number">1</p>
+		</div>
+		<div class="user" style="background: #348930">
+			<p class="title">Funcionários</p>
+			<p class="number">1</p>
+		</div>
+	</div>
+</body>
+</html>
